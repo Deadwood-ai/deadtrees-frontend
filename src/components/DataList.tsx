82ece053bc966a943dbf190936de0413ee48a995
JesJehle <jesvajnajehle@gmail.com>
2024-03-06 11:46:36 +0100

    Add icons and update styling in DataList and DatasetDetails components

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,7 +1,7 @@
 import { Avatar, List, Space, Typography, Button } from "antd";
 import { useState } from "react";
 import { Dataset } from "../types/dataset";
-import { DownloadOutlined } from "@ant-design/icons";
+import { DownloadOutlined, HeartOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 
 export default function DataList({ data }: { data: Dataset }) {
@@ -13,29 +13,14 @@ export default function DataList({ data }: { data: Dataset }) {
       itemLayout="vertical"
       dataSource={data}
       renderItem={(item, index) => (
-        <List.Item
-          key={index}
-          style={{
-            margin: 0,
-          }}
-        >
+        <List.Item key={index}>
           <div
             key={index}
-            className="flex p-3 bg-gray-200 rounded-md hover:bg-gray-300 transition duration-150 ease-in-out"
-            // onMouseEnter={() => {}}
+            className="flex p-3 bg-white rounded-md hover:bg-gray-200 transition duration-150 ease-in-out"
             onClick={() => navigate(`/dataset/${item.uuid}`)}
           >
             <Avatar size={64} src="https://avatars.githubusercontent.com/u/8186664?v=7" />
-            <div
-              className="flex flex-1 flex-col justify-between pl-3"
-              // style={{
-              //   paddingLeft: "12px",
-              //   display: "flex",
-              //   flexDirection: "column",
-              //   justifyContent: "space-between",
-              //   flex: 1,
-              // }}
-            >
+            <div className="flex flex-1 flex-col justify-between pl-3">
               <Text strong>{item.file_name}</Text>
               <div>
                 <Space size="small">
@@ -48,10 +33,8 @@ export default function DataList({ data }: { data: Dataset }) {
                 </Space>
               </div>
             </div>
-            <div className="flex flex-col justify-between p-3">
-              <Button type="default" size="small" icon={<DownloadOutlined />}>
-                {/* {item.platform} */}
-              </Button>
+            <div className="flex pr-3">
+              <Button type="default" size="small" icon={<HeartOutlined />}></Button>
             </div>
           </div>
         </List.Item>
--- src/pages/DatasetDetails.tsx
+++ src/pages/DatasetDetails.tsx
@@ -1,8 +1,9 @@
-import { Button, Col, Row } from "antd";
+import { Button, Col, Row, Tag, Typography } from "antd";
 import { useParams, useNavigate } from "react-router-dom";
 import { useData } from "../state/DataProvider";
 
 import Map from "../components/Map";
+import { ArrowLeftOutlined, BackwardFilled, EnvironmentFilled, EnvironmentOutlined } from "@ant-design/icons";
 
 export default function DatasetDetails() {
   const navigate = useNavigate();
@@ -10,6 +11,7 @@ export default function DatasetDetails() {
   const data = useData();
   const dataset = data.data?.find((d) => d.uuid === id);
   console.log(dataset);
+  const date = new Date(dataset?.aquisition_date);
   return (
     <Row
       style={{
@@ -18,15 +20,61 @@ export default function DatasetDetails() {
       }}
     >
       <Col className="flex flex-col align-middle p-3  w-96 pr-4 ">
-        <Button onClick={() => navigate(-1)} type="primary">
-          Back
-        </Button>
         {/* {id} */}
         {dataset ? (
-          <div>
-            <h1>{dataset.file_name}</h1>
-            <p>{dataset.aquisition_date}</p>
-            <p>{dataset.file_id}</p>
+          <div className="p-2">
+            <div>
+              <Button
+                size="large"
+                shape="circle"
+                onClick={() => navigate(-1)}
+                // type="primary"
+                icon={<ArrowLeftOutlined />}
+              ></Button>
+            </div>
+            <div className="bg-white p-4 mt-4 rounded-md">
+              <div className="flex items-center pb-4">
+                <EnvironmentOutlined style={{ fontSize: 24 }} className="pr-2" />
+                <Typography.Title style={{ margin: 0 }} level={4}>
+                  {dataset.file_name}
+                </Typography.Title>
+              </div>
+
+              <div className="flex p-1">
+                <Typography.Text className="pr-2">Acquisition Date: </Typography.Text>
+                <Typography.Text strong>
+                  {date.toLocaleString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </Typography.Text>
+              </div>
+              <div className="flex p-1">
+                <Typography.Text className="pr-2">Content Type: </Typography.Text>
+                <Typography.Text strong>{dataset.content_type}</Typography.Text>
+              </div>
+            </div>
+            <div className="bg-white p-4 mt-4 rounded-md">
+              <div className="flex justify-between p-2">
+                <Typography.Text style={{ margin: 0 }}>
+                  <Typography.Text className="pr-2">License: </Typography.Text>
+                </Typography.Text>
+                <Tag color="blue">{dataset.license}</Tag>
+              </div>
+              <div className="flex justify-between p-2">
+                <Typography.Text style={{ margin: 0 }}>
+                  <Typography.Text className="pr-2">Platform: </Typography.Text>
+                </Typography.Text>
+                <Tag color="blue">{dataset.platform}</Tag>
+              </div>
+              <div className="flex justify-between p-2">
+                <Typography.Text style={{ margin: 0 }}>
+                  <Typography.Text className="pr-2">Status: </Typography.Text>
+                </Typography.Text>
+                <Tag color="blue">{dataset.status}</Tag>
+              </div>
+            </div>
           </div>
         ) : (
           <div>Loading...</div>
