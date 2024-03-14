ca2ddd218fa30dc418112e84a3df125406f86960
JesJehle <jesvajnajehle@gmail.com>
2024-03-14 22:23:44 +0100

    parsing bbox

---
+++ src/utils/parseBBox.tsx
@@ -0,0 +1,16 @@
+type BBox = [[number, number], [number, number]]; // [minLongitude, minLatitude, maxLongitude, maxLatitude]
+
+function parseBBox(boxString: string): BBox | null {
+  const regex = /BOX\((\d+\.\d+) (\d+\.\d+),(\d+\.\d+) (\d+\.\d+)\)/;
+  const matches = boxString.match(regex);
+
+  if (!matches) return null;
+
+  const [minLongitude, minLatitude, maxLongitude, maxLatitude] = matches.slice(1).map(Number);
+
+  return [
+    [minLatitude, minLongitude],
+    [maxLatitude, maxLongitude],
+  ];
+}
+export default parseBBox;
