217e470d5e79fdfb6bbc5c179d57877cc7d45abf
JesJehle <jesvajnajehle@gmail.com>
2024-03-19 13:46:52 +0100

    Refactor parseBBox to handle negative coordinates

--- src/utils/parseBBox.tsx
+++ src/utils/parseBBox.tsx
@@ -1,12 +1,14 @@
 type BBox = [[number, number], [number, number]]; // [minLongitude, minLatitude, maxLongitude, maxLatitude]
 
 function parseBBox(boxString: string): BBox | null {
-  const regex = /BOX\((\d+\.\d+) (\d+\.\d+),(\d+\.\d+) (\d+\.\d+)\)/;
+  const regex = /BOX\((-?\d+\.\d+) (-?\d+\.\d+),(-?\d+\.\d+) (-?\d+\.\d+)\)/;
   const matches = boxString.match(regex);
 
   if (!matches) return null;
 
-  const [minLongitude, minLatitude, maxLongitude, maxLatitude] = matches.slice(1).map(Number);
+  const [minLongitude, minLatitude, maxLongitude, maxLatitude] = matches
+    .slice(1)
+    .map(Number);
 
   return [
     [minLatitude, minLongitude],
