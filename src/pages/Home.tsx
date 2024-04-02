d86158c8c29a0a0d2afdc59fbbbef1bda262f175
JesJehle <jesvajnajehle@gmail.com>
2024-04-02 22:55:18 +0200

    fix text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -330,8 +330,8 @@ const GetInContact = () => {
         Want to join?
       </p>
       <p className="m-auto max-w-xl pt-8 text-center text-lg text-gray-500">
-        Do you have high-resolution 20cm orthoimagery and would like to
-        participate in this project?
+        {`Do you have high-resolution (<20cm) orthoimagery and would like to
+        participate in this project?`}
       </p>
       <div className="flex justify-center pt-8">
         <Button
