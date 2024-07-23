b8eb390fa875e5f79bf25cdf440ab848cbc18860
JesJehle <jesvajnajehle@gmail.com>
2024-07-23 16:03:43 +0200

    adding config file

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const Configuration = {
+  USE_LOCAL_SERVER: false,
+  LOCAL_SERVER_URL: "http://localhost:3000",
+  PRODUCTION_SERVER_URL: "https://api.deadtrees.earth",
+  TESTING: false,
+};
