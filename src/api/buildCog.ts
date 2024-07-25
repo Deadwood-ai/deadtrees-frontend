4573d75b5c2c097e435a4399f3de39d49a4df86e
JesJehle <jesvajnajehle@gmail.com>
2024-07-25 13:47:32 +0200

    adding api

---
+++ src/api/buildCog.ts
@@ -0,0 +1,32 @@
+import { Settings } from "../config";
+
+const buildCog = async (dataset_id: number, token: string) => {
+  try {
+    const res = await fetch(`${Settings.API_URL}/datasets/${dataset_id}/build-cog`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json", // Specify that the content type is JSON
+      },
+      body: JSON.stringify({
+        overviews: 8, // Number of overview layers to generate
+        resolution: 0.04, // Target resolution in the COG file
+        profile: "webp", // Profile type for the COG (e.g., webp, jpeg, etc.)
+        force_recreate: false, // Flag to force recreate the COG if it already exists
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data = await res.json(); // Parse the response body as JSON
+    console.log("Build-cog response data:", data);
+    return data;
+  } catch (error) {
+    console.error("Build-cog error:", error);
+    throw error;
+  }
+};
+
+export default buildCog;
--- src/api/uploadFile.ts
+++ src/api/uploadFile.ts
@@ -3,19 +3,27 @@ import { Settings } from "../config";
 const uploadFile = async (file: any, token: string) => {
   console.log("file in uploadFile", file);
   const formData = new FormData();
-  formData.append("file", file);
+  formData.append("file", file.originFileObj);
+
   try {
     const res = await fetch(Settings.API_URL + "/datasets", {
+      method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
       },
-      method: "POST",
       body: formData,
     });
-    console.log("res", res);
-    return res;
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data = await res.json(); // Parse the response body as JSON
+    console.log("Upload dataset response data:", data);
+    return data;
   } catch (error) {
-    console.error("Upload error:", error);
+    console.error("Upload dataset error:", error);
+    throw error;
   }
 };
 
