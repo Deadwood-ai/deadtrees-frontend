3414387cde1083ffb8d2534b0d3e60ee49c3e666
JesJehle <jesvajnajehle@gmail.com>
2025-06-06 11:46:41 +0200

    Update getDeadwoodCOGUrl to change base URL for COG data retrieval
    - Replaced the previous Supabase URL with a new data source URL for accessing COG files, ensuring continued data availability.
    - Commented out the old URL for reference, maintaining clarity in the codebase.

--- src/utils/getDeadwoodCOGUrl.tsx
+++ src/utils/getDeadwoodCOGUrl.tsx
@@ -1,7 +1,8 @@
 import { supabase } from "../hooks/useSupabase";
 
 const getDeadwoodCOGUrl = (year: string | null) => {
-  const baseUrl = "https://ijuphmnaebfdzsfrnsrn.supabase.co/storage/v1/object/public/COG/DE-";
+  // const baseUrl = "https://ijuphmnaebfdzsfrnsrn.supabase.co/storage/v1/object/public/COG/DE-";
+  const baseUrl = "https://data2.deadtrees.earth/products/v1/DE-";
   const url = `${baseUrl}${year}-COG.tif`;
 
   // console.log("public URL of COG:", url);
